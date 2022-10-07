a8e74d1d6808eab2210e99fa9515fe4db8d7cf2c
Esenova Anar <aesenova@siroca.com>
2022-10-07 11:12:56 +0600

    [MultiThreading] Initial commit

---
+++ MultiThreading.Task2.Chaining/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * 2.	Write a program, which creates a chain of four Tasks.
+ * First Task – creates an array of 10 random integer.
+ * Second Task – multiplies this array with another random integer.
+ * Third Task – sorts this array by ascending.
+ * Fourth Task – calculates the average value. All this tasks should print the values to console.
+ */
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task2.Chaining
+{
+    class Program
+    {
+        private const int RandomMaxValue = 1000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".Net Mentoring Program. MultiThreading V1 ");
+            Console.WriteLine("2.	Write a program, which creates a chain of four Tasks.");
+            Console.WriteLine("First Task – creates an array of 10 random integer.");
+            Console.WriteLine("Second Task – multiplies this array with another random integer.");
+            Console.WriteLine("Third Task – sorts this array by ascending.");
+            Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
+            Console.WriteLine();
+
+            ExecuteTaskChain();
+
+        }
+
+        static void ExecuteTaskChain()
+        {
+            Task<int[]> taskRandom = Task.Run(() =>
+            {
+                var rnd = new Random();
+                var intArray = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    intArray[i] = rnd.Next(0, RandomMaxValue);
+                    Console.WriteLine($"Source array element[{i}] = {intArray[i]}");
+                }
+                return intArray;
+            });
+
+            var taskMultiply = taskRandom.ContinueWith(antecedent => //Task<int[]>???
+            {
+                var rnd = new Random();
+                var inArray = antecedent.Result;
+                var multipleOn = rnd.Next(5, 10);
+                Console.WriteLine($"Multiple array on = {multipleOn}");
+                for (int i = 0; i < 10; i++)
+                {
+                    inArray[i] = inArray[i] * multipleOn;
+                    Console.WriteLine($"Multiplied array element[{i}] = {inArray[i]}");
+                }
+                return inArray;
+            });
+
+            var taskSort = taskMultiply.ContinueWith(antecedent =>
+            {
+                var inArray = antecedent.Result;
+                var outArray = inArray.OrderBy(x=>x).ToArray();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Sorted array element[{i}] = {outArray[i]}");
+                }
+
+                return outArray;
+            });
+
+            var taskAverage = taskSort.ContinueWith(antecedent =>
+            {
+                var inArray = antecedent.Result;
+                var average = inArray.Average(x=>x);
+                return average;
+            });
+
+            Console.WriteLine($"Average of sorted = {taskAverage.Result}");//this line waits for taskAverage.Result, and of course for all chaining tasks
+
+        }
+
+        public int[] MultiplyArray(int[] inArray)
+        {
+            var rnd = new Random();
+            var multipleOn = rnd.Next(0, 10);
+            for (int i = 0; i < 10; i++)
+            {
+                inArray[i] = inArray[i] * multipleOn;
+                Console.WriteLine($"Multiplied array element[{i}] = {inArray[i]}");
+            }
+            return inArray;
+        }
+    }
+}
---
+++ MultiThreading.Task3.Matrixes/Multipliers/MatricesMultiplierParallel.cs
@@ -0,0 +1,45 @@
+﻿using MultiThreading.Task3.MatrixMultiplier.Matrices;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task3.MatrixMultiplier.Multipliers
+{
+    public class MatricesMultiplierParallel : IMatricesMultiplier
+    {
+        public IMatrix Multiply(IMatrix m1, IMatrix m2)
+        {
+            var resultMatrix = new Matrix(m1.RowCount, m2.ColCount);
+            for (long i = 0; i < m1.RowCount; i++)
+            {
+                //for (byte j = 0; j < m2.ColCount; j++)
+                //{
+                //    long sum = 0;
+                //    for (byte k = 0; k < m1.ColCount; k++)
+                //    {
+                //        sum += m1.GetElement(i, k) * m2.GetElement(k, j);
+                //    }
+
+                //    resultMatrix.SetElement(i, j, sum);
+                //}
+
+                Parallel.For(0, m2.ColCount, j => resultMatrix.SetMultipliedElement(m1, m2, i, (int)j));
+
+            };
+
+            return resultMatrix;
+        }
+    }
+
+    public static class MatrixHelper
+    {
+        public static void SetMultipliedElement(this Matrix matrix, IMatrix m1, IMatrix m2, long i, int j)
+        {
+            long sum = 0;
+            for (byte k = 0; k < m1.ColCount; k++)
+            {
+                sum += m1.GetElement(i, k) * m2.GetElement(k, j);
+            }
+
+            matrix.SetElement(i, j, sum);
+        }
+    }
+}
